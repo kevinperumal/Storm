e0037cf457653368ce348a9b7b8911ff148cd92a
Kevin Perumal <kevin.perumal@gmail.com>
2016-04-09 18:48:07 -0400

    Adds network calls to forecast.io

--- Storm/ViewController.swift
+++ Storm/ViewController.swift
@@ -14,19 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentHumidityLabel: UILabel?
     @IBOutlet weak var currentPrecipitationLabel: UILabel?
 
+    private let forecastAPIKey = "b7402ea41ff33993076ef2bc0851b099"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
-        let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
-        let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject]{
-            
-            let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
         
-            currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
-            currentHumidityLabel?.text =  "\(currentWeather.humidity)%"
-            currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
-        }
-    
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        // Use NSURLSession
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: configuration)
+        
+        // Request object
+        let request = NSURLRequest(URL: forecastURL!)
+        
+        let dataTask = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) -> Void in
+            print(data)
+            print("im on a background thread")
+        })
+        print("im on the main thread")
+        dataTask.resume()
     }
 
     override func didReceiveMemoryWarning() {
